c540e66b76d2e22b8a6e10d45be222a6db209983
Tejas <tejas14all@gmail.com>
2021-07-04 10:51:18 +0530

    js first

---
+++ js/myscript.js
@@ -0,0 +1,17 @@
+var a = 12;
+var b = 10;
+var ans = a*b;
+//console.log(ans);
+/* */
+
+
+document.write(ans); // document --> object --> HTML page 
+
+
+
+/*
+    window =>  window.alert();  alert() 
+    document =>
+*/
+
+ 
